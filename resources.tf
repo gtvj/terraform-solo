c9c30cafb7ecea5f18e8e6b7451c8dfa81109dc0
Gwyn Jones <m@gwnjns.com>
2018-06-17 11:53:51 +0100

    Add subnet to VPC
    This adds a subnet to my VPC.

--- resources.tf
+++ resources.tf
@@ -8,3 +8,13 @@ resource "aws_vpc" "fifteen-volts-virtual-network" {
     Name = "fifteen-volts"
   }
 }
+
+resource "aws_subnet" "fifteen-volts-subnet-one" {
+  cidr_block = "${cidrsubnet(aws_vpc.fifteen-volts-virtual-network.cidr_block, 3, 1)}"
+  vpc_id = "${aws_vpc.fifteen-volts-virtual-network.id}"
+  availability_zone = "eu-west-2a"
+
+  tags {
+    Name = "fifteen-volts"
+  }
+}
